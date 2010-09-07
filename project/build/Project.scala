f536a699a0ddc5d67d3546a966fa9a6f8c827c87
razvanc <razvanc@razubuntu.(none)>
2010-09-06 21:00:41 -0400

    all kinda of changes

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,14 +2,7 @@ import sbt._
 
 class Razxml(info: ProjectInfo) extends DefaultProject(info)
 {
-  lazy val hi = task { println("Hello World"); None }
-
-  override def outputDirectoryName = "bin"
-
   override def mainScalaSourcePath = "src"
-//  override def mainResourcesPath = "resources"
-
   override def testScalaSourcePath = "test_src"
-//  override def testResourcesPath = "test-resources"
 }
 
